4b09e7d7fc130afddf121d18da053623e17c189e
Utkarsh <utkarsh12march2004@gmail.com>
2024-01-27 09:14:36 +0530

    Your commit message here

--- src/Pages/Session2022.jsx
+++ src/Pages/Session2022.jsx
@@ -3,18 +3,10 @@ import React from 'react';
 const Session2022 = () => {
   return (
     <div>
-    <h1 class="text-center text-5xl font-bold text-gray-500 font-bold  py-8 relative">
-        <span class="absolute left-0 w-[40%] h-1 bg-blue-200 top-1/2 transform -translate-y-1/2"></span>
-        Session 2022
-        <span class="absolute right-0 w-[40%] h-1 bg-blue-200 top-1/2 transform -translate-y-1/2"></span>
-    </h1>
-<div className='flex flex-row items-center  justify-between'>
-      <img src="https://placements.iiitl.ac.in/static/media/gearmtec23.4d2d6f26.png" className='flex items-center justify-center w-[20%]'></img>
-      <img src="https://placements.iiitl.ac.in/static/media/gearmtec23.4d2d6f26.png" className='flex items-center justify-center w-[20%]'></img>
-
-      <img src="https://placements.iiitl.ac.in/static/media/gearmtec23.4d2d6f26.png" className='flex items-center justify-center w-[20%]'></img>
-</div>
-          </div>
+      <h1>Session 2022</h1>
+      <p>This is the content for Session 2022. Replace this with your actual content.</p>
+      <img src="https://placements.iiitl.ac.in/static/media/gear22.1e843aa5.png"></img>
+    </div>
   );
 };
 
